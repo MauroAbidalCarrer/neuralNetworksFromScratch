adb3ecfa765a7198e47464f9dad738c9dc1c0c36
Mauro ABIDAL CARRER <mauroabidal@yahoo.fr>
2023-07-07 19:52:51 +0200

    First implementation of a layer, ReLu and SoftMax(no backPropagation yet)

---
+++ activation_functions.py
@@ -0,0 +1,22 @@
+import numpy as np
+import nnfs
+# from nnfs.datasets import spiral_data
+
+# Sets the random seed to 0 and does some other stuff to make the output repetable
+nnfs.init()
+
+class Relu:
+
+    def forward(inputs):
+        return np.maximum(0, inputs)
+    
+class SoftMax:
+
+    def forward(inputs):
+        exp_inputs = np.exp(inputs)                                      # exponantiate the input.
+        return exp_inputs / np.sum(exp_inputs, axis=1, keepdims=True)   # devide it by its sum.
+                                                                        # Since we are using a batch if imputs (i.e a matrix), 
+                                                                        # "axis=1" specifies that we want to add only the components of the vectors.
+                                                                        # "keepdims=True" specifies that we want the result to keep the same number of dimensions(but not the same shape).
+                                                                        # This way we get a matrix as output of the sum instead of a an array.
+                                                                        # Having a matrix of shape (nb_inputs, 1) allows us to use it as denomintor.
---
+++ single_layer.py
@@ -0,0 +1,45 @@
+import sys
+import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
+# Sets the random seed to 0 and does some other stuff to make the output repetable
+nnfs.init()
+from activation_functions import *
+
+
+class Layer:
+
+    def __init__(self, input_size, nb_neurons, activation_function):
+        self.nb_inputs = input_size
+        self.nb_neurons = nb_neurons
+        # Create a matrix of shape(nb_neurons, nb_inputs) with random values.
+        # Since we are using batches of inputs and performing matrix multiplication on them and that
+        # because matMul performs the dot product on the rows of the first matrix and the columms of the second instead of row/row,
+        # we would need to transpose() the weight matrix for every pass.
+        # So instead, we make the matrix of shape (input_size, nb_neurons).
+        self.weights = np.random.randn(input_size, nb_neurons)
+        # The parameter of the funciton is in parenthesis because it is a tuple of size one.
+        # print(self.weights)
+        self.biases = np.zeros((nb_neurons))
+        self.activation_function = activation_function
+
+    def forward_pass(self, inputs):
+        self.output = np.dot(inputs, self.weights)                        # Perform dot product of the weights on each inputs(Should maybe use @ ?)
+                                                                            # No need to transpose the weight matrix since its shape is (input_size, nb_neurons).
+        self.output = self.output + self.biases                             # Add the biases on each row/input.
+        self.output = self.activation_function.forward(self.output)         # Apply the activation function.
+
+
+
+#main
+
+# Create dataset
+samples, targets = spiral_data(samples=100, classes=3)
+
+layer1 = Layer(2, 3, Relu)
+# print(X.__len__())
+layer1.forward_pass(samples)
+# print(layer1.output)
+layer2 = Layer(3, 3, SoftMax)
+layer2.forward_pass(layer1.output)
+print(layer2.output)
