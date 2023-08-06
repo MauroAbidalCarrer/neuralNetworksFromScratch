e9e8df733e622f1b115556c5bcbf96d87b8666bd
Mauro Abidal Carrer <mauroabida@yahoo.fr>
2023-08-06 13:20:32 -0400

    Implemented datasets loading

--- main.py
+++ main.py
@@ -20,37 +20,40 @@ logs_file = open('logs.txt', '+a')
 
 # Download dataset if it's not already there.
 if not os.path.isfile(ZIP_FILE):
-    print('Downloading {FASHION_MNIST_DOWNLOAD_URL} and saving as {ZIP_FILE}...')
+    print('Downloading ' + FASHION_MNIST_DOWNLOAD_URL + ' and saving as ' + ZIP_FILE + '...', end="", flush=True)
     urllib.request.urlretrieve(FASHION_MNIST_DOWNLOAD_URL, ZIP_FILE)
     print('Done!')
 
-
+print('yes: ' + FASHION_MNIST_DOWNLOAD_URL)
 # Extract the .zip if there is no fashion_mnist_images folder.
 if not os.path.isdir('fashion_mnist_images'):
-    print('Unzipping images...')
+    print('Unzipping images...', end="", flush=True)
     with ZipFile(ZIP_FILE) as zip_images:
         zip_images.extractall(FOLDER)
         print('Done!')
 
 # Load datasets.
 def load_dataset(dataset_type):
+    """Type must be either 'train' or 'test'."""
+    print('Loading ' + dataset_type + 'ing dataset... ', end="", flush=True)
     inputs = []
     expected_outputs = []
-    """type must be either 'train' or 'test'."""
     for label_class in os.listdir(os.path.join(FOLDER, dataset_type)):
         folder_path = os.path.join(FOLDER, dataset_type, label_class)
         for image_name in os.listdir(folder_path):
-            image = cv2.imread(os.path.join(folder_path, image_name), cv2.IMREAD_UNCHANGED)
+            image_path = os.path.join(folder_path, image_name)
+            # print('Loading ' + image_path)
+            image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
             inputs.append(image)
-            expected_outputs.append(expected_outputs)
+            expected_outputs.append(label_class)
     inputs = np.array(inputs).astype('float32') / 255. - 0.5
-    return (inputs, np.array(expected_outputs).astype('uint8'))
+    # print('Going to return')
+    expected_outputs = np.array(expected_outputs)
+    print('done!')
+    return (inputs, expected_outputs.astype('uint8'))
 
-print('Loading training dataset... ')
 training_inputs, training_outputs = load_dataset('train')
-print('Loading test dataset... ')
 test_inputs, test_outputs = load_dataset('test')
-print(training_inputs[0])
 
 
 # Create model
